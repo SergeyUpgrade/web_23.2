2e0c0d435b6bf430f6d7785f66822b2ee7b54c1b
Сергей <sergey.vasilev.98@mail.ru>
2024-09-16 21:27:02 +0300

    new work, but forget commit stages:(

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home
+from catalog.views import products_list, contacts, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", products_list, name="products_list"),
     path("contacts/", contacts, name="contacts"),
+    path("catalog/<int:pk>/", product_detail, name="product_detail")
 ]
