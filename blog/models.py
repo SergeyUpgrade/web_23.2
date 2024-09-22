73535bd65d823d413d75449bae44adf910fe4933
Сергей <sergey.vasilev.98@mail.ru>
2024-09-22 17:23:36 +0300

    created new model blog

--- blog/models.py
+++ blog/models.py
@@ -1,37 +1,24 @@
 from django.db import models
 
-NULLABLE = {"blank": True, "null": True}
+class Blog(models.Model):
 
+    name = models.CharField(max_length=100,verbose_name="Заголовок",)
 
-class Product(models.Model):
+    slug = models.CharField(max_length=150, verbose_name='slug', null=True, blank=True)
 
-    name = models.CharField(
-        max_length=100,
-        verbose_name="Заголовок",
-        help_text="Введите заголовок",
-    )
+    content = models.TextField(verbose_name="Содержание")
 
-    slug = models.CharField(max_length=150, verbose_name='slug')
-    content = models.TextField(
-        verbose_name="Содержание", help_text="Введите текст", **NULLABLE
-    )
-    image = models.ImageField(
-        upload_to="catalog/image",
-        verbose_name="Изображение",
-        help_text="Загрузите изображение",
-        **NULLABLE
-    )
+    image = models.ImageField(upload_to="catalog/image",verbose_name="Изображение",help_text="Загрузите изображение")
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    is_published = models.BooleanField(default=True)
-    views_count = models.IntegerField(default=0, verbose_name='просмотры')
 
-    class Meta:
-        verbose_name = "Товар"
-        verbose_name_plural = "Товары"
-        ordering = ["name", "price"]
+    is_published = models.BooleanField(default=True)
+
+    views_count = models.IntegerField(default=0, verbose_name='просмотры')
 
     def __str__(self):
         return self.name
-
+    class Meta:
+        verbose_name = 'блог'
+        verbose_name_plural = 'блоги'
 # Create your models here.
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 from django.views.generic import ListView, DetailView, TemplateView
 
@@ -18,17 +17,3 @@ class ContactsTemplateView(TemplateView):
     template_name = "catalog/contacts.html"
 
 
-#def contacts(request):
-#    if request.method == "POST":
-#        name = request.POST.get("name")
-#phone = request.POST.get("phone")
-##        message = request.POST.get("message")
-#        print(f"{name} ({phone}): {message}")
-#
-#    return render(request, "contacts.html")
-
-
-#def product_detail(request, pk):
-#    product = get_object_or_404(Product, pk=pk)
- #   context = {"product": product}
-#    return render(request, "product_detail.html", context)
