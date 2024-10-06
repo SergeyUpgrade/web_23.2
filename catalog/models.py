5ccc9e2fad9fbe35e1933f39832e0272b4a2b830
Сергей <sergey.vasilev.98@mail.ru>
2024-10-06 20:03:15 +0300

    create class Version

--- catalog/models.py
+++ catalog/models.py
@@ -59,3 +59,43 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Version(models.Model):
+    product = models.ForeignKey(
+        Product,
+        related_name="versions",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Версия",
+    )
+
+    version_num = models.PositiveIntegerField(
+        verbose_name="Номер версии",
+        help_text="Укажите номер версии продукта",
+        default=0,
+        null=True,
+        blank=True,
+    )
+
+    version_name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование версии",
+        help_text="Введите наименование версии продукта",
+        default="",
+        null=True,
+        blank=True,
+    )
+
+    active = models.BooleanField(
+        verbose_name="Признак текущей версии",
+        help_text="Введите признак текущей версии продукта",
+        default=False
+    )
+
+    class Meta:
+        verbose_name = "Версия продукта"
+        verbose_name_plural = "Версии продукта"
+
+    def __str__(self):
+        return f"{self.product} {self.version_num} {self.version_name} {self.active}"
