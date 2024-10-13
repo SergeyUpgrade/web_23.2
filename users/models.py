6ea08b2a68bb42609111af733634b03b9839525e
Сергей <sergey.vasilev.98@mail.ru>
2024-10-13 22:57:02 +0300

    problem with verification

--- users/models.py
+++ users/models.py
@@ -12,6 +12,7 @@ class User(AbstractUser):
     phone = models.CharField(max_length=35, verbose_name='Телефон', blank=True, null=True, help_text='Введите номер телефона')
     country = models.CharField(max_length=20, verbose_name='Страна', blank=True, null=True, help_text='Страна проживания')
     avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Аватар', blank=True, null=True, help_text='Ваш аватар')
+    token = models.CharField(max_length=100, verbose_name='Token', blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
